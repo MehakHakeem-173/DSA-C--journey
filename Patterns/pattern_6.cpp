ead3c06c898dd79e0162c65d9dedc6f0b683b9a2
Mehak Hakeem <mehakhakeem52@@gmail.com>
2026-02-26 21:14:02 +0500

    pattern_6

--- Patterns/pattern_6.cpp
+++ Patterns/pattern_6.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 
-void Pattern1(int N){ //this function is for inverse triangle
-    for(int i=0;i<N;i++){ //outer loop for rows
-        for(int j=N;j>i+1;j--){ //inner loop for columns
-            cout<<"*"<<" "; 
+void Pattern1(int N){ //this function is for a inverse triangle of numbers
+    for(int i=1;i<N;i++){ //outer loop for rows
+        for(int j=i;j<=N;j++){ //inner loop for columns
+            cout<<j<<" "; 
         }
         cout<<endl; //end of a row , only for outer loop
     }
