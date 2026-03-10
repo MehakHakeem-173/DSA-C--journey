3a89dcf22c54f41a4c44ea995c17c37b79740d08
Mehak Hakeem <mehakhakeem52@gmail.com>
2026-03-10 21:04:43 +0500

    pattern 12

---
+++ Patterns/pattern_12.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+//making function
+void pattern12(int N){
+    //number of space b/w numbers will be 2*N-1
+    int space = 2*N-1;
+    //outer loop for number of rows
+    for(int i=1;i<=N;i++){
+        //inner loop for numbers before space
+        for(int j=0;j<=i;j++){
+            cout<<j;
+        }
+        //inner loop for space
+        for(int j=1;j<=space;j++){
+            cout<<" ";
+        }
+        //inner loop for number after space
+        for(int j=i;j>=0;j--){
+            cout<<j;
+        }
+        //end of the row
+        cout<<endl;
+        //modification of veriable space
+        space -=2;
+    }
+}
+
+int main(){
+    //size of pattern
+    int N= 5;
+    //calling the function
+    pattern12(N);
+    return 0;
+}
