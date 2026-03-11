fdc3257a740d9f49ef1a68c229922e49458d7843
Mehak Hakeem <mehakhakeem52@gmail.com>
2026-03-11 22:05:41 +0500

    pattern 16

--- Patterns/pattern_16.cpp
+++ Patterns/pattern_16.cpp
@@ -1,18 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-pattern16(int n){
+//function for pattern
+void pattern16(int n){
+    //outer loop for number of rows
     for(int i=0;i<n;i++){
+        //define the character based on each row
         char ch = 'A' + i;
+        //inner row for number of columns
         for(int j=0;j<=i;j++){
+            //print the characters
             cout<<ch<<" ";
         }
+        //end of each row
         cout<<endl;
     }
 }
 
 int main(){
+    //size of pattern
     int N = 5;
+    //calling the function
     pattern16(N);
     return 0;
 }
