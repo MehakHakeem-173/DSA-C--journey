2bf15edf261d8fe9bbb0005ed556bed59e40603f
Mehak Hakeem <mehakhakeem52@gmail.com>
2026-03-25 22:20:00 +0500

    bubble sorting

---
+++ Sorting/Bubble_sorting.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class BubbleSort{
+    public:
+//function for sorting the array
+    void Bubble_Sort(vector<int>& arr){
+     int n = arr.size();
+
+     //nested loop to itterate over the array
+     for (int i=n-1;i>=0;i--){
+        for (int j=0;j<=i;j++){
+
+            //swap the values if the later one is lower
+            if (arr[j] > arr[j+1]){
+                swap ( arr[j], arr [j+1]);
+            }
+        }
+     }
+
+     //printing array after sorting
+     cout << "after sorting using bubble sorting \n";
+     for (int num : arr){
+        cout << num << " ";
+     }
+     cout << endl;
+    }     
+};
+
+int main(){
+    // enter the dynamic array (vector)
+    vector<int> arr = {35, 65, 32, 23, 9, 12};
+    
+    //printing array before sorting
+    cout << "before using bubble sorting \n";
+    for ( int num : arr){
+        cout << num << " ";
+    }
+
+    //end of arry
+    cout << endl;
+
+    //making a class BubbleSort
+    BubbleSort sorter;
+
+    //calling the function inside the BubbleSort
+    sorter.Bubble_Sort(arr);
+    return 0;
+}
