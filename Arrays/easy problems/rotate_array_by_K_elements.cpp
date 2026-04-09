e154701d834fd6e131051e1c3ff3ec6a3722e311
Mehak Hakeem <mehakhakeem52@gmail.com>
2026-04-09 09:56:17 +0500

    apply changes

--- Arrays/easy problems/rotate_array_by_K_elements.cpp
+++ Arrays/easy problems/rotate_array_by_K_elements.cpp
@@ -15,36 +15,46 @@ class Solution{
         //temp array to store the elements in
         int temp[k];
         
-        //loop to print the value after those we arerotating
-        for (int i=n-k; i<n; i++){ //start from 4 to 5
-            temp[i-n+k] = arr[i]; //index zero of temp is index 3 of arr which has value four
+        //loop to print the value after those we are rotating
+        for (int i=n-k; i<n; i++){ //start from 3 & 4
+            temp[i-(n-k)] = arr[i]; //for index 0 and 1, store index 3 and 4 
         }
 
-        //
-        for(int i=n-k-1; i>=0; i--){ //from index 2 to zero, (value 2 to 1)
-            arr[i+k] = arr[i];
+
+        for(int i=n-k-1; i>=0; i--){ //start from index 2, go to index 0
+            arr[i+k] = arr[i]; // shift 0, 1, and 2 to 2, 3 and 4 , leaving 0 and 1 empty
         }
 
-        //
-        for (int i=0; i<k; i++){ //
+        
+        for (int i=0; i<k; i++){ //start from the empty 0 and 1 and put the array temp in there
             arr[i] = temp[i];  
         }
     }
 
     void rotateLeft(int arr[] , int n, int k){
+
+        //if array size is zero , return
         if(n == 0) return;
+
+        //take modulus if value of k excedes n
         k = k % n;
+
+        //temparory array with size k
         int temp[k];
         
-        for (int i=0; i<k; i++){
-            temp[i] = arr[i];
-        }
-        for(int i=k; i<n; i++){
-            arr[i-k] = arr[i];
+        
+        for (int i=0; i<k; i++){//go from index 0 and 1 of arr
+            temp[i] = arr[i]; // store the value of arr[0 and 1] in temp [0 and 1]
         }
 
-        for (int i=0; i<k; i++){
-            arr[n-k+i] = temp[i];
+
+        for(int i=k; i<n; i++){ //start from 2 to 4 of arr 
+            arr[i-k] = arr[i];//put the value of 2, 3, and 4, in 0, 1, and 2
+        }
+
+
+        for (int i=0; i<k; i++){ //start from 0 and 1 of temp
+            arr[n-k+i] = temp[i]; //put the value of temp in arr 3 and 4
         }
     }
 };
