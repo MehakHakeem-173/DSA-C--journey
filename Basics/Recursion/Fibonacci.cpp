f1f3b353c63326e272ec6b69e6422ac9e37bcc0b
Mehak Hakeem <mehakhakeem52@gmail.com>
2026-04-23 13:33:13 +0500

    apply changes

---
+++ Basics/Recursion/Fibonacci.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 5;
+    if(n == 0){
+        cout << 0;
+    }
+    if (n == 1){
+        cout << 0 << " " << 1 << endl;
+    }
+}
--- Basics/Recursion/check_if_string_is_palindrom.cpp
+++ Basics/Recursion/check_if_string_is_palindrom.cpp
@@ -1,6 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//function to check the string
+bool isPalindrom(string s){
+
+    int left = 0, right = s.length() - 1;
+
+    //loop to find non-characters
+    while (left < right){
+
+        //if index is not a character, move farward
+        if (!isalnum(s[left]))
+        left++;
+
+        //same but move backward
+        else if (!isalnum(s[right]))
+        right--;
+
+        //if characters doesn't match, return false
+        else if (tolower(s[right]) != tolower(s[left]))
+        return false;
+
+        //else the loop runs perfactly and the string is a palindrom
+        else{
+            right--;
+            left++;
+        }
+        return true;
+    }
+}
+
 int main(){
+
+    //string str
     string str = "ABCDCBA";
-    
+
+    //calling the function
+    bool ans = isPalindrom(str);
+
+    //printing the result
+    if (ans == true){
+        cout << "palindrom";
+    }
+    else{
+        cout << "not a palindrom";
+    }
+    return 0;
 }
