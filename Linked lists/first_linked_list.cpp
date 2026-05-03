04e65b25a0c0468b4a7584e8fef3c013c0ef7da1
Mehak Hakeem <mehakhakeem52@gmail.com>
2026-05-03 19:25:15 +0500

    first linked list, apply changes

---
+++ Linked lists/first_linked_list.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1){
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+int main(){
+    vector<int> arr = {2, 5, 3, 4};
+    Node* y = new Node(arr[0]);
+    cout << y << endl;
+    cout << y->data << endl;
+    return 0;
+}
