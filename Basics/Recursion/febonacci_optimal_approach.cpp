ac32ccd57ef54598e5bf7b5fbfe0aaa3877a1792
Mehak Hakeem <mehakhakeem52@gmail.com>
2026-05-12 20:01:57 +0500

    fibonacci optimal approach

--- Basics/Recursion/febonacci_optimal_approach.cpp
+++ Basics/Recursion/febonacci_optimal_approach.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int fibonacci(int N){
+    if (N <= 1) {
+        return N;
+    }
+    int last = fibonacci(N-1);
+    int Slast = fibonacci(N-2);
+
+    return last + Slast;
+}
+
 int main () {
-    
+    int N = 5;
+    for (int i=0; i<=N; i++){
+        cout << fibonacci(i) << " ";
+    }
+    return 0;
 }
