e159f2aae1386f1a4e9d5522acd09a383889a3c9
Mehak Hakeem <mehakhakeem52@gmail.com>
2026-07-08 20:18:05 +0500

    stock buy and sell optimal opproach

---
+++ Arrays/Medium problems/stock_buy_and_sell_optimal_approach.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//class to find the maximum profit
+class Solution{
+    public:
+    int stockBuySell(vector<int> arr){
+        
+    }
+};
+
+int main(){
+    Solution sol;
+    //array to find the maximum in
+    vector<int> arr = {7, 1, 5, 3, 6, 4};
+
+    //calling the function
+    cout << sol.stockBuySell(arr);
+    return 0;
+}
