b317ba705d5df4ed30a0f43fe40d0e917f9a0d33
Mehak Hakeem <mehakhakeem52@gmail.com>
2026-08-24 16:32:50 +0500

    find no in sorted array through iterative approach

---
+++ binary search/fine_no_in_sorted_array.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+
+    public:
+
+    int binarySearch(vector<int> arr, int target){
+        int n= arr.size();
+        int low = 0; 
+        int high = n - 1;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] == target) return mid;
+
+            else if(arr[mid] < target) low = mid + 1;
+            
+            else high = mid - 1;
+        }
+
+        return -1;
+    }
+};
+int main(){
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
+
+    int target = 6;
+    Solution sol;
+    int ind = sol.binarySearch(arr, target);
+
+    if(ind == -1) cout << "the number is not found in the array" << endl;
+    else cout << "the number is found at index: " << ind << endl;
+
+    return 0;
+}
